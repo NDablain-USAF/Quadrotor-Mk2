9a64f99a2c00271048b1e5b59d0ab18c4413fb81
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-06-18 08:05:16 -0400

    Update Main.h

--- src/Main.h
+++ src/Main.h
@@ -11,14 +11,15 @@
 uint8_t Transmit(uint8_t Slave_Address, uint8_t Register_Address, uint8_t Register_Data);
 // Read: Reads up to 256 bytes of data from the specified slave address
 // IMPORTANT, is looking for an array of data, pass address if only reading a single value
-// Returns 1 if successful
-uint8_t Read(uint8_t Slave_Address, uint8_t Register_Address, uint8_t data[]);
+// Returns 8 if successful, lower numbers indicate stage of error
+uint8_t Read(uint8_t Slave_Address, uint8_t Register_Address, uint8_t *data, uint8_t length);
 // A reproduction of the delay() function, prevents overwhelming the serial monitor
 void Detain(uint16_t length);
 // Use to group Transmit calls when sensor is being set up 
+// Returns 2 if successful, lower numbers indicate stage of error
 uint8_t Initialize_BMP390(uint32_t PAR_T[3]);
 // Use this to read the Temperature data off the sensor
-uint16_t Read_BMP390(uint32_t PAR_T[3]);
+uint32_t Read_BMP390(uint32_t PAR_T[3]);
 // Use to group MCU one time register setups prior to running while loop
 void Initialize_ATMEGA328P();
 
@@ -27,6 +28,6 @@ void Initialize_ATMEGA328P();
 #define BMTR_TEMP_DATA 0x07 // First bit of temperature data
 #define BMTR_OSR 0x1C // Output sample rate register
 #define BMTR_ODR 0x1D // Output data rate register
-#define BMTR_CON_START 0x32 // Start register of factory calibration data
+#define BMTR_CON_START 0x31 // Start register of factory calibration data
 
 #endif
