e2cc186897c1339aeae1743faa62926a3ff6ae0f
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-02 15:14:44 -0400

    Update spi.h

--- src/Examples/SPI/spi.h
+++ src/Examples/SPI/spi.h
@@ -6,7 +6,23 @@
 
 #define F_CPU 20000000UL
 
-void SPI_Init();
-void Transmit(long outbound);
+void SPI_Init(){
+	DDRB |= (1<<PINB5) | (1<<PINB3) | (1<<PINB2); // Set SCK, MOSI, and SS as outputs
+	PORTB |= (1<<PINB2); // Set SS high
+	SPCR |= (1<<SPE) | (1<<MSTR) | (1<<SPR0); // Enables SPI, sets AVR as master, sets clock to f_osc/16
+}
+
+void Transmit(long outbound){
+	char buffer[10];
+	int n = sprintf(buffer,"%ld",outbound);
+	PORTB &= ~(1<<PINB2); // Set B2 LOW
+	for (int i=0;i<n;i++){
+		SPDR = buffer[i]; // Put data into data register
+		while (!(SPSR & (1<<SPIF))){asm("");} // Wait for flag to be set in status register to continue
+	}
+	SPDR = 10; // Line feed
+	while (!(SPSR & (1<<SPIF))){asm("");}
+	PORTB |= (1<<PINB2); // Set B2 HIGH
+}
 
 #endif
