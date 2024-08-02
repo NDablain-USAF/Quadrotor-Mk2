8d5f44726b25b435e7d045b848a804abe1ece134
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-02 08:33:04 -0400

    Update SPI.c

--- src/Examples/SPI.c
+++ src/Examples/SPI.c
@@ -1,20 +1,24 @@
 #include <avr/io.h>
 
-volatile char g_Timer_Flag = 0; 
+#define F_CPU 16000000UL
 
-void Transmit(char outbound);
+void Transmit(unsigned char outbound);
 
 int main(void){
-    DDRB |= (1<<DDB5) | (1<<DDB3); // Set SCK and MOSI as outputs
-    SPCR |= (1<<SPE) | (1<<MSTR) | (1<<SPR0); // Enables SPI, sets AVR as master, sets clock to f_osc/16
-    while (1) {
-	if (g_Timer_Flag==1){Transmit(5);}
-    }
+	DDRB |= (1<<PINB5) | (1<<PINB3) | (1<<PINB2); // Set SCK, MOSI, and SS as outputs
+	PORTB |= (1<<PINB2);
+	SPCR |= (1<<SPE) | (1<<MSTR) | (1<<SPR1) | (1<<SPR0); // Enables SPI, sets AVR as master, sets clock to f_osc/128
+	while (1) {
+		unsigned char x = 1;
+		Transmit(x);
+		//volatile int i = 0;
+		//while(i<60000){i++;}
+	}
 }
 
-void Transmit(char outbound){
-	SPDR = outbound; // Put data into data register
-	while (!(SPSR & (1<<SPIF))); // Wait for flag to be set in status register to continue
-	SPSR &= 0b01111111; // Ensure SPIF flag is reset
-	g_Timer_Flag = 0;
+void Transmit(unsigned char outbound){
+	PORTB &= ~(1<<PINB2); // Set B2 LOW
+	SPDR = (outbound+48); // Put data into data register
+	while (!(SPSR & (1<<SPIF))){asm("");} // Wait for flag to be set in status register to continue
+	PORTB |= (1<<PINB2); // Set B2 HIGH
 }
