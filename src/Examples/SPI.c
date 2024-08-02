eba6f9ff900f4b32b34da704cff9389c9e1ce96c
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-02 12:13:09 -0400

    Update SPI.c
    Test sprintf and sending longer numbers

--- src/Examples/SPI.c
+++ src/Examples/SPI.c
@@ -1,22 +1,27 @@
 #include <avr/io.h>
+#include <cstdio.h>
 
 #define F_CPU 16000000UL
 
-void Transmit(unsigned char outbound);
+void Transmit(long outbound);
 
 int main(void){
 	DDRB |= (1<<PINB5) | (1<<PINB3) | (1<<PINB2); // Set SCK, MOSI, and SS as outputs
 	PORTB |= (1<<PINB2); // Set SS high
 	SPCR |= (1<<SPE) | (1<<MSTR) | (1<<SPR0); // Enables SPI, sets AVR as master, sets clock to f_osc/16
 	while (1) {
-		unsigned char x = 1;
-		Transmit(x);
+		int16_t x = 1000;
+		Transmit((long)x);
 	}
 }
 
-void Transmit(unsigned char outbound){
+void Transmit(long outbound){
+	char buffer[20];
+	uint8_t n = sprintf(buffer,%d,outbound);
 	PORTB &= ~(1<<PINB2); // Set B2 LOW
-	SPDR = (outbound+48); // Put data into data register
-	while (!(SPSR & (1<<SPIF))){asm("");} // Wait for flag to be set in status register to continue
+	for (uint8_t i=0;i<n;i++){	
+		SPDR = (buffer[i]+48); // Put data into data register
+		while (!(SPSR & (1<<SPIF))){asm("");} // Wait for flag to be set in status register to continue
+	}
 	PORTB |= (1<<PINB2); // Set B2 HIGH
 }
