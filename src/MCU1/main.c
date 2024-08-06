1fdb28114c750c84a04b21c99cec9a0d7d11c0fa
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-06 14:50:29 -0400

    Create main.c

---
+++ src/MCU1/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(){
+  Initialize_ATMEGA328P();
+  struct BMI088 IMU = {0};
+  char BMI_Status = Initialize_BMI088(IMU);
+  while(1){
+    if (g_Acc_Flag==1){Read_Acc(IMU);}
+    if (g_Gyr_Flag==1){Read_Gyr(IMU);}
+  }
+
+  return 0;
+}
