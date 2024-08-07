52fe7d6823571da4412bd1174e9a51de95b07bb9
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-07 07:58:18 -0400

    Update main.c

--- src/MCU1/main.c
+++ src/MCU1/main.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 int main(){
-  Initialize_ATMEGA328P();
-  struct BMI088 IMU = {0};
-  char BMI_Status = Initialize_BMI088(IMU);
-  while(1){
-    if (g_Acc_Flag==1){Read_Acc(IMU);}
-    if (g_Gyr_Flag==1){Read_Gyr(IMU);}
-  }
+	Initialize_ATMEGA328P();
+	struct BMI088 IMU = {0};
+	char BMI_Status = Initialize_BMI088(&IMU);
+	while(1){
+		if (BMI_Status==9){
+			if (g_Acc_Flag==1){
+				g_Acc_Flag = 0;
+				Read_Acc(&IMU);
+			}	
+			if (g_Gyr_Flag==1){
+				g_Gyr_Flag = 0;
+				Read_Gyr(&IMU);
+			}
+		}
+	}
 
-  return 0;
+	return 0;
 }
