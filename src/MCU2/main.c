f6f6fe949cc2120cc1982808312150d25a330f86
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-19 14:43:53 -0400

    Update main.c

--- src/MCU2/main.c
+++ src/MCU2/main.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 int main(void){
+	I2C_Init();
 	SPI_Init();
 	LoRa_Init();
     while(1){
+		unsigned char data = 187;
 		LoRa_Read();
+		if ((TWCR&(1<<TWINT))==(1<<TWINT)){
+			unsigned char status = I2C_Slave_Transmit(data);
+		}
     }
 }
