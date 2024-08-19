82dd1202f9f5c6c034df056e2738b85ff269f6c9
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-08-19 07:48:14 -0400

    Update main.c

--- src/MCU2/main.c
+++ src/MCU2/main.c
@@ -1,18 +1,9 @@
 #include "main.h"
 
-int main(){
-  float PAR_T[3];
-  float PAR_P[11];
-  Initialize_ATMEGA328P();
-  unsigned char Status = Initialize_BMP390(PAR_T, PAR_P); // Group sensor setup in this function
-  while(1){
-    if (bmtr_flag==1){
-      if (Status==2){ // If initialization was successful
-        volatile float Height = Read_BMP390(PAR_T, PAR_P);
-      }
+int main(void){
+	SPI_Init();
+	LoRa_Init();
+    while(1){
+		LoRa_Read();
     }
-
-  }
-
-  return 0;
 }
