87a735ec88388011c4e07b9cab3678c427f8fff9
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-09-02 10:05:14 -0400

    Update main.c

--- src/MCU2/main.c
+++ src/MCU2/main.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 int main(void){
-	I2C_Init();
+	ATMega328P_Init();
 	SPI_Init();
+	I2C_Init();
 	LoRa_Init();
+	//BMP_Init();
+	int passed_variable = 0;
     while(1){
-		unsigned char data = 187;
-		LoRa_Read();
-		if ((TWCR&(1<<TWINT))==(1<<TWINT)){
-			unsigned char status = I2C_Slave_Transmit(data);
-		}
+		LoRa_Read(&passed_variable);
+		unsigned char data[2] = {0,0};
+		data[0] = (unsigned char)passed_variable;
+		data[1] = passed_variable>>8;
+		unsigned char status = I2C_Slave_Transmit(data,2);
+		//unsigned char data;
+		//SPI_Read_Register(1,1,BMP_ID,&data,1);
+		//SPI_Transmit_Long_Data_Visualizer((long)passed_variable,5);
     }
 }
