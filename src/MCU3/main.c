7ca7a3c9c166f9baf339864f26fad6160bf980a0
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-09-08 12:02:27 -0400

    Update main.c

--- src/MCU3/main.c
+++ src/MCU3/main.c
@@ -3,9 +3,13 @@
 int main(void){
 	ATMega328P_Init();
 	LoRa_Init();
+	struct Potentiometer Pot1 = {POT_MUX1,PORTD6,0};
+	struct Potentiometer Pot2 = {POT_MUX2,PORTD7,0};
+	struct Potentiometer Pot3 = {POT_MUX3,PORTD4,0};
     while(1){
-		unsigned char message = 20;
-        LoRa_Transmit(message);
+		Measure_Pot(&Pot1,&Pot2,&Pot3);
+		Run_Radio(&Pot1,&Pot2,&Pot3);
+		
 		Detain(100000UL);
     }
 	return 0;
